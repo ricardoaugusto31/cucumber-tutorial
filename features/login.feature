89a5f1c9b67bd817f030770e4e598f6e30ab4636
Ricardo Yan Augusto <ricardo.yan@ti.ukdw.ac.id>
2025-05-31 01:00:48 +0700

    Second commit

--- features/login.feature
+++ features/login.feature
@@ -3,8 +3,42 @@ Feature: User Login
   I want to login using a valid email
   So that I can access the application
 
-  Scenario: Successful login with valid details
-    Given the user is on the login page
-    When the user enters a valid username and password
-    And the user clicks the login button
-    Then the user should see a success message
+  # Scenario: Successful login with valid details
+  #   Given the user is on the login page
+  #   When the user enters a valid username and password
+  #   And the user clicks the login button
+  #   Then the user should see a success message
+
+  # Scenario: Failed login with invalid credential
+  #   Given the user is on the login page
+  #   When the user enters an invalid username and password
+  #   And the user clicks the login button
+  #   Then the user should see a failed message
+
+  # Scenario: Successfully adding an item to cart
+  #   Given the user is on the login page
+  #   And the user is on the item page
+  #   When the user add item to the cart
+  #   And the user in the item list 
+  #   Then item should be seen in the item page
+
+  # Scenario: Successfully removing an item from cart
+  #   Given the user is on the login page
+  #   And the user is on the item page
+  #   When the user add item to the cart
+  #   And the user in the item list
+  #   When the user remove item to the cart
+  #   Then item shouldn't be seen in the item page
+
+  # Scenario: Successfully sorting items by price(low to high)
+  #   Given the user is on the login page
+  #   And the user is on the item page
+  #   When the user sorts items by price low to high
+  #   Then items should be sorted by price low to high
+
+  # Scenario: Successfully logging out
+  #   Given the user is on the login page
+  #   And the user is on the item page
+  #   When the user clicks on the slider menu
+  #   And the user clicks on the logout button
+  #   Then the user should be redirected to the login page
--- features/step_definitions/step.js
+++ features/step_definitions/step.js
@@ -6,26 +6,175 @@ setDefaultTimeout(30000);
 
 let driver;
 
-Given("the user is on the login page", async function () {
-  driver = new Builder().forBrowser("chrome").build();
-  await driver.get("https://www.saucedemo.com/");
-});
+// Scenario: Successful login with valid details
+// Given("the user is on the login page", async function () {
+//   driver = new Builder().forBrowser("chrome").build();
+//   await driver.get("https://www.saucedemo.com/");
+// });
 
-When("the user enters a valid username and password", async function () {
-  await driver.findElement(By.id("user-name")).sendKeys("standard_user");
-  await driver.findElement(By.id("password")).sendKeys("secret_sauce");
-});
+// When("the user enters a valid username and password", async function () {
+//   await driver.findElement(By.id("user-name")).sendKeys("standard_user");
+//   await driver.findElement(By.id("password")).sendKeys("secret_sauce");
+// });
 
-When("the user clicks the login button", async function () {
-  await driver.findElement(By.id("login-button")).click();
-});
+// When("the user clicks the login button", async function () {
+//   await driver.findElement(By.id("login-button")).click();
+// });
 
-Then("the user should see a success message", async function () {
-  const message = await driver
-    .wait(until.elementLocated(By.className("title")), 5000)
-    .getText();
-  expect(message).to.equal("Products");
-  const item = await driver.findElement(By.id("item_4_img_link"));
-  expect(item).to.exist;
-  await driver.quit();
-});
+// Then("the user should see a success message", async function () {
+//   const message = await driver
+//     .wait(until.elementLocated(By.className("title")), 5000)
+//     .getText();
+//   expect(message).to.equal("Products");
+//   const item = await driver.findElement(By.id("item_4_img_link"));
+//   expect(item).to.exist;
+//   await driver.quit();
+// });
+
+// Scenario: Failed login with invalid credential
+// Given("the user is on the login page", async function () {
+//   driver = new Builder().forBrowser("chrome").build();
+//   await driver.get("https://www.saucedemo.com/");
+// });
+
+// When("the user enters an invalid username and password", async function () {
+//   await driver.findElement(By.id("user-name")).sendKeys("testing");
+//   await driver.findElement(By.id("password")).sendKeys("testing123");
+// });
+
+// When("the user clicks the login button", async function () {
+//   await driver.findElement(By.id("login-button")).click();
+// });
+
+// Then("the user should see a failed message", async function () {
+//   const item = await driver.findElement(By.className("error"));
+//   expect(item).to.exist;
+//   await driver.quit();
+// });
+
+// Scenario: Successfully adding an item to cart
+// Given("the user is on the login page", async function () {
+//   driver = new Builder().forBrowser("chrome").build();
+//   await driver.get("https://www.saucedemo.com/");
+//   await driver.findElement(By.id("user-name")).sendKeys("standard_user");
+//   await driver.findElement(By.id("password")).sendKeys("secret_sauce");
+//   await driver.findElement(By.id("login-button")).click();
+// });
+
+// When("the user is on the item page", async function () {
+//   const item = await driver.findElement(By.id("item_4_img_link"));
+//   expect(item).to.exist;
+//   await driver.findElement(By.id("item_4_img_link")).click();
+// });
+
+// When("the user add item to the cart", async function () {
+//   const addButton = await driver.findElement(By.id("add-to-cart"));
+//   await addButton.click();
+// });
+
+// Then("item should be seen in the item page", async function () {
+//   const cartBadge = await driver.findElement(By.className("shopping_cart_badge"));
+//   const badgeText = await cartBadge.getText();
+//   expect(badgeText).to.equal("1");
+//   await driver.quit();
+// });
+
+
+// Scenario: Successfully removing an item from cart
+// Given("the user is on the login page", async function () {
+//   driver = new Builder().forBrowser("chrome").build();
+//   await driver.get("https://www.saucedemo.com/");
+//   await driver.findElement(By.id("user-name")).sendKeys("standard_user");
+//   await driver.findElement(By.id("password")).sendKeys("secret_sauce");
+//   await driver.findElement(By.id("login-button")).click();
+// });
+
+// When("the user is on the item page", async function () {
+//   const item = await driver.findElement(By.id("item_4_img_link"));
+//   expect(item).to.exist;
+//   await driver.findElement(By.id("item_4_img_link")).click();
+// });
+
+// When("the user add item to the cart", async function () {
+//   const addButton = await driver.findElement(By.id("add-to-cart"));
+//   await addButton.click();
+// });
+
+// When("the user in the item list", async function () {
+//   const cartBadge = await driver.findElement(By.className("shopping_cart_badge"));
+//   const badgeText = await cartBadge.getText();
+//   expect(badgeText).to.equal("1");
+//   await driver.findElement(By.id("shopping_cart_container")).click();
+// });
+
+// When("the user remove item to the cart", async function () {
+//   const removeButton = await driver.findElement(By.id("remove-sauce-labs-backpack"));
+//   await removeButton.click();
+// });
+
+// Then("item shouldn't be seen in the item page", async function () {
+//   const item = await driver.findElements(By.id("item_4_img_link"));
+//   expect(item).to.have.lengthOf(0);
+//   await driver.quit();
+// });
+
+// Scenario: Successfully sorting items by price(low to high)
+// Given("the user is on the login page", async function () {
+//   driver = new Builder().forBrowser("chrome").build();
+//   await driver.get("https://www.saucedemo.com/");
+//   await driver.findElement(By.id("user-name")).sendKeys("standard_user");
+//   await driver.findElement(By.id("password")).sendKeys("secret_sauce");
+//   await driver.findElement(By.id("login-button")).click();
+// });
+
+// When("the user is on the item page", async function () {
+//   const item = await driver.findElement(By.id("item_4_img_link"));
+//   expect(item).to.exist;
+// });
+
+// When("the user sorts items by price low to high", async function () {
+//   const sortDropdown = await driver.findElement(By.className("product_sort_container"));
+//   await sortDropdown.click();
+//   const option = await driver.findElement(By.css('option[value="lohi"]'));
+//   await option.click();
+// });
+
+// Then("items should be sorted by price low to high", async function () {
+//     const item = await driver.findElement(By.id("item_2_img_link"));
+//     expect(item).to.exist;
+//     await driver.quit();
+// });
+
+// Scenario: Successfully logging out
+// Given("the user is on the login page", async function () {
+//   driver = new Builder().forBrowser("chrome").build();
+//   await driver.get("https://www.saucedemo.com/");
+//   await driver.findElement(By.id("user-name")).sendKeys("standard_user");
+//   await driver.findElement(By.id("password")).sendKeys("secret_sauce");
+//   await driver.findElement(By.id("login-button")).click();
+// });
+
+// When("the user is on the item page", async function () {
+//   const item = await driver.findElement(By.id("item_4_img_link"));
+//   expect(item).to.exist;
+// });
+
+// When("the user clicks on the slider menu", async function () {
+//   const menuButton = await driver.findElement(By.id("react-burger-menu-btn"));
+//   await menuButton.click();
+//   await driver.sleep(2000); 
+// });
+
+// When("the user clicks on the logout button", async function () {
+//   const logoutButton = await driver.findElement(By.id("logout_sidebar_link"));
+//   await logoutButton.click();
+// });
+
+// Then("the user should be redirected to the login page", async function () {
+//   const loginButton = await driver.wait(
+//     until.elementLocated(By.id("login-button")),
+//     5000 // waktu tunggu 5 detik
+//   );
+//   expect(loginButton).to.exist;
+//   await driver.quit();
+// });
